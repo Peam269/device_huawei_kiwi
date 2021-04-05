2f520dcd96d5232103663a742a59638c82567144
althafvly <althafvly@gmail.com>
2021-04-05 18:55:46 +0530

    kiwi: Make sure proprietary binaries exists before compilation
    Signed-off-by: althafvly <althafvly@gmail.com>
    Change-Id: If027d537495c90f0703eb57ed55bff7b647f7036

--- device.mk
+++ device.mk
@@ -17,7 +17,7 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-$(call inherit-product-if-exists, vendor/huawei/kiwi/kiwi-vendor.mk)
+$(call inherit-product, vendor/huawei/kiwi/kiwi-vendor.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
