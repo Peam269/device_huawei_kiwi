febee832113ba56c7cd26b4088edd2f89314b300
Roman Kiryanov <rkir@google.com>
2023-08-28 21:57:59 +0200

    kiwi: replace audio@2.0-service with audio.service Legacy service name, use android.hardware.audio.service instead
    Bug: 153579444
    Test: presubmit
    Signed-off-by: Roman Kiryanov <rkir@google.com>
    Merged-In: I4c05accb86dd0c9cd1b3fef3d84e0d54b2afc6a1
    Change-Id: I6b55b4b1db2577f4176c99dca7021cfe78e13307

--- device.mk
+++ device.mk
@@ -58,7 +58,7 @@ TARGET_SCREEN_WIDTH := 1080
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
-    android.hardware.audio@2.0-service \
+    android.hardware.audio.service \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.broadcastradio@1.0-impl \
     audio.primary.msm8916 \
