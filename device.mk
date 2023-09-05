84bdddc034ba4d663ec7244a55e1d9ebeb077abe
Emil <emilwagenplast@gmail.com>
2023-09-05 17:21:56 +0200

    kiwi: add camera provider service * This fixes camera crashing in A11

--- device.mk
+++ device.mk
@@ -109,6 +109,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl-legacy \
+    android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl-legacy \
     camera.msm8916 \
     libmm-qcamera \
