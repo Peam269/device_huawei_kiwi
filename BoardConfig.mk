78bc5103e1fa8a003a605643a34983da80226524
Emil <emilwagenplast@gmail.com>
2023-09-15 00:48:10 +0200

    kiwi: add shim for sensors.kiwi.so *This fixes some sensors in A11
      MultiHal: dlerror(): dlopen failed: cannot locate symbol "get_app_info" referenced by "/system/vendor/lib64/sensors.kiwi.so"...

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -183,7 +183,8 @@ TARGET_LD_SHIM_LIBS := \
     /system/vendor/bin/signinfolistener|libshim_signinfolistener.so \
     /system/vendor/lib/libmmcamera_hdr_gb_lib.so|/system/vendor/lib/libmmqjpeg_codec.so \
     /system/vendor/lib/libdmd.so|libshim_cutils.so \
-    /system/vendor/lib64/hw/fingerprint.msm8916.so|libshim_cutils.so
+    /system/vendor/lib64/hw/fingerprint.msm8916.so|libshim_cutils.so \
+    /system/vendor/lib64/sensors.kiwi.so|libshim_cutils.so
 
 # Vendor Init
 TARGET_INIT_VENDOR_LIB := libinit_kiwi
